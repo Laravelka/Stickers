3a064fe05d02f4249d9eb25c3c7fb8da7a8040c8
root <root@347312-stickshops.tmweb.ru>
2020-09-11 22:24:54 +0300

    Updates

---
+++ precache-manifest.799a53b2b3e183ef5d606c2602fce305.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4f9383bd64985715dde796ac9593e0",
+    "url": "/AnimeTest/index.html"
+  },
+  {
+    "revision": "4c14a5496bbb4f671048",
+    "url": "/AnimeTest/static/css/2.9f63dd42.chunk.css"
+  },
+  {
+    "revision": "1861a4f8bc18003121b2",
+    "url": "/AnimeTest/static/css/main.842843b0.chunk.css"
+  },
+  {
+    "revision": "4c14a5496bbb4f671048",
+    "url": "/AnimeTest/static/js/2.38e6b273.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AnimeTest/static/js/2.38e6b273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1861a4f8bc18003121b2",
+    "url": "/AnimeTest/static/js/main.88aca184.chunk.js"
+  },
+  {
+    "revision": "3bd6b335ede2461b954f",
+    "url": "/AnimeTest/static/js/runtime-main.d03219a1.js"
+  },
+  {
+    "revision": "26709d6957665e4e28c173c96421572d",
+    "url": "/AnimeTest/static/media/1.26709d69.png"
+  },
+  {
+    "revision": "eadb269aeb1861479fb8b00f7dd5fde2",
+    "url": "/AnimeTest/static/media/1.eadb269a.png"
+  },
+  {
+    "revision": "f425e055bb754a50e9a3ad0db5ef9b05",
+    "url": "/AnimeTest/static/media/10.f425e055.png"
+  },
+  {
+    "revision": "fc755b7045a19e7c094d0b21f3763468",
+    "url": "/AnimeTest/static/media/10.fc755b70.png"
+  },
+  {
+    "revision": "5c344c437dee989efdc2d300c35d35aa",
+    "url": "/AnimeTest/static/media/11.5c344c43.png"
+  },
+  {
+    "revision": "4d97dd1b34123b02259c12acea4e73c2",
+    "url": "/AnimeTest/static/media/12.4d97dd1b.png"
+  },
+  {
+    "revision": "2c2927b7150f637ee57cee68f252329f",
+    "url": "/AnimeTest/static/media/13.2c2927b7.png"
+  },
+  {
+    "revision": "982931fc91a3dd68e3ac795e4eea8a1b",
+    "url": "/AnimeTest/static/media/14.982931fc.png"
+  },
+  {
+    "revision": "482ef09ff5990ab8a39436af5ef07678",
+    "url": "/AnimeTest/static/media/15.482ef09f.png"
+  },
+  {
+    "revision": "71fd4d96da244f84778a0502c722b4af",
+    "url": "/AnimeTest/static/media/16.71fd4d96.png"
+  },
+  {
+    "revision": "71483affadc25948643ea7a40e053aa6",
+    "url": "/AnimeTest/static/media/2.71483aff.png"
+  },
+  {
+    "revision": "c31726ab782728676d0cff1c6e40a3fc",
+    "url": "/AnimeTest/static/media/2.c31726ab.png"
+  },
+  {
+    "revision": "517adb5540d1b201245b133d401810dd",
+    "url": "/AnimeTest/static/media/3.517adb55.png"
+  },
+  {
+    "revision": "c6ad33afa78e4d1e82fe5b5873bbcb7b",
+    "url": "/AnimeTest/static/media/3.c6ad33af.png"
+  },
+  {
+    "revision": "39b317e66ee47063f7c1f168307bc84b",
+    "url": "/AnimeTest/static/media/4.39b317e6.png"
+  },
+  {
+    "revision": "63df1b1a19492ae99959356e88a650f7",
+    "url": "/AnimeTest/static/media/4.63df1b1a.png"
+  },
+  {
+    "revision": "b5e0f1fd5232c9080c7228355169021a",
+    "url": "/AnimeTest/static/media/5.b5e0f1fd.png"
+  },
+  {
+    "revision": "f80f31d242f1ecb2089a80d189b2b434",
+    "url": "/AnimeTest/static/media/5.f80f31d2.png"
+  },
+  {
+    "revision": "0aae8fff9adcc26e7d4e9bb0bb761875",
+    "url": "/AnimeTest/static/media/6.0aae8fff.png"
+  },
+  {
+    "revision": "a051ae3d453ede0495bbb0e26bccf25e",
+    "url": "/AnimeTest/static/media/6.a051ae3d.png"
+  },
+  {
+    "revision": "18f84cc078485365d48665059fcf363a",
+    "url": "/AnimeTest/static/media/7.18f84cc0.png"
+  },
+  {
+    "revision": "5361fa08950b3f04be78ae52f74e1d91",
+    "url": "/AnimeTest/static/media/7.5361fa08.png"
+  },
+  {
+    "revision": "039535fc740dde3fc5b3dfdcec4918b4",
+    "url": "/AnimeTest/static/media/8.039535fc.png"
+  },
+  {
+    "revision": "fa24699737fa5ec572f3baa14b1cf6e5",
+    "url": "/AnimeTest/static/media/8.fa246997.png"
+  },
+  {
+    "revision": "c2fc271b4444e9685479928aa7729ce7",
+    "url": "/AnimeTest/static/media/9.c2fc271b.png"
+  },
+  {
+    "revision": "ebfaf0170ab6959b1b453cd5add7483f",
+    "url": "/AnimeTest/static/media/9.ebfaf017.png"
+  },
+  {
+    "revision": "a4882cba24164ca9b61430477bf2ff5f",
+    "url": "/AnimeTest/static/media/result.a4882cba.png"
+  }
+]);
